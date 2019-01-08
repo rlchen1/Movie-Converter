4e56b0a480ade3979ef69d59a289a9e2b8f41463
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 15:58:49 -0800

    Update DIPs.c

--- DIPs.c
+++ DIPs.c
@@ -1,13 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/*                                                                   */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* DIPs.c: source file for DIP operations                            */
-/*                                                                   */
-/*********************************************************************/
-
 #include <stdlib.h>
 #include <assert.h>
 #include "DIPs.h"
