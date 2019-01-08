e98ea60b881cdf086208d10f15e0bf13fe6342e8
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 15:59:34 -0800

    Update FileIO.h

--- FileIO.h
+++ FileIO.h
@@ -1,13 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/*                                                                   */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* FileIO.h: header file for basic file manipulations                */
-/*                                                                   */
-/*********************************************************************/
-
 #ifndef FILEIO_H
 #define FILEIO_H
 
