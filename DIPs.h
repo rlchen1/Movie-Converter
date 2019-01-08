98d24243974c7f7454de598ef5bceb849e9c3a6e
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 15:59:11 -0800

    Update DIPs.h

--- DIPs.h
+++ DIPs.h
@@ -1,13 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/*                                                                   */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* DIPs.h: header file for DIP operations                            */
-/*                                                                   */
-/*********************************************************************/
-
 #ifndef DIPS_H
 #define DIPS_H
 
