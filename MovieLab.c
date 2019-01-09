32a2c5aedf6e4bd62a917eb02dc2559d4a48317a
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 16:03:11 -0800

    Update MovieLab.c

--- MovieLab.c
+++ MovieLab.c
@@ -1,14 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/* Modified by Mihnea Chirila for EECS 22 Fall 2018                                                            */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* MovieLab.c: source file for the main function                     */
-/*                                                                   */
-/* Please use this template for your HW5.                            */
-/*********************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
