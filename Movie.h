15985bdeb0d21808105eced6dc53b862cd390c34
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 16:02:48 -0800

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -1,13 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/*                                                                   */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* Movie.h: header file for basic movie manipulations                */
-/*                                                                   */
-/*********************************************************************/
-
 #ifndef MOVIE_H
 #define MOVIE_H
 
