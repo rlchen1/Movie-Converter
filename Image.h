91dc57489871619d7c1dd7ee85520e1bf60e391a
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 16:00:24 -0800

    Update Image.h

--- Image.h
+++ Image.h
@@ -1,13 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/*                                                                   */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* Image.h: header file for basic image manipulations                */
-/*                                                                   */
-/*********************************************************************/
-
 #ifndef IMAGE_H
 #define IMAGE_H
 
