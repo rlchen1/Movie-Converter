39e304d3f52594b46f7a342abe319735fa690247
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 16:00:43 -0800

    Update ImageList.c

--- ImageList.c
+++ ImageList.c
@@ -1,13 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/*                                                                   */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* ImageList.c: source file for image list manipulations             */
-/*                                                                   */
-/*********************************************************************/
-
 #include <stdlib.h>
 #include <assert.h>
 #include "ImageList.h"
