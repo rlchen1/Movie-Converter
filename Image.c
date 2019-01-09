213ffe81dee5a8d5665fbc10a2ebc2a4614e06b3
Robert Chen <44920058+rlchen1@users.noreply.github.com>
2019-01-08 16:00:02 -0800

    Update Image.c

--- Image.c
+++ Image.c
@@ -1,13 +1,3 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Fall 2017                     */
-/*                                                                   */
-/* Author: Tim Schmidt                                               */
-/* Date: 11/09/2017                                                  */
-/*                                                                   */
-/* Image.c: source file for basic image manipulations                */
-/*                                                                   */
-/*********************************************************************/
-
 #include <stdlib.h>
 #include <assert.h>
 #include "Image.h"
